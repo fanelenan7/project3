9f576e5d848c164a390f5f24b7b633cf73c26d6f
Justin Shin <jjs3b@virginia.edu>
2017-03-15 21:50:47 -0400

    currently final stuff, points to heroku

--- js/app.js
+++ js/app.js
@@ -31,13 +31,27 @@ function RouterFunction($stateProvider) {
       url: "/",
       templateUrl: "js/ng-views/index.html",
       controller: "bookarooIndexController",
-      controllerAs: "vm"
+      controllerAs: "vm",
+      // clear animation and bookmarks
+      onEnter: function() {
+        document.querySelectorAll(".bookmark").forEach((bookmark) => {
+          bookmark.style.display = "none"
+        })
+      }
     })
     .state("bookaroo", {
       url: "/books/:id",
       templateUrl: "js/ng-views/show.html",
       controller: "bookarooShowController",
-      controllerAs: "vm"
+      controllerAs: "vm",
+      // animation trigger on enter
+      onEnter: function() {
+        document.querySelectorAll(".bookmark").forEach((bookmark) => {
+          bookmark.style.display = "flex"
+          let newElement = bookmark.cloneNode(true)
+          bookmark.parentNode.replaceChild(newElement, bookmark)
+        })
+      }
     })
 }
 Array.prototype.randomElement = function () { //added to prototype so I can randomly choose book without ruining scope in the show controller
@@ -45,11 +59,11 @@ Array.prototype.randomElement = function () { //added to prototype so I can rand
 }
 
 function bookarooBookFactoryFunction($resource) {
-  let url = "http://localhost:3000/books/:id"
+  let url = "https://bookaroo.herokuapp.com/books/:id"
   return $resource(url)
 }
 function bookarooGenreFactoryFunction($resource) {
-  let url = "http://localhost:3000/genres/:id"
+  let url = "https://bookaroo.herokuapp.com/genres/:id"
   return $resource(url)
 }
 function bookarooIndexControllerFunction(bookarooGenreFactory) {
@@ -58,24 +72,31 @@ function bookarooIndexControllerFunction(bookarooGenreFactory) {
 }
 function bookarooShowControllerFunction(bookarooBookFactory, $stateParams, $state, $scope) {
   this.books = bookarooBookFactory.query()
+
   this.books.$promise.then((books)=>{ //*.query() is asynchronous and has not finished retrieving the value. using then to retrieve value
     this.book = books.filter( function(book){ //finding every instance where the subject matches what we chose
       return $stateParams.id ? book.subject == $stateParams.id : true //checks for a category, if none, randomly choose from all books
     }).randomElement()
   })
-  $scope.reloadRoute = function() {
-    $state.reload();
+
+  // jjs: this is handled via jquery below and can now be deleted
+  // $scope.reloadRoute = function() {
+  //   $state.reload();
+  // }
+
+  //jjs: the two bookmark-links that went out of scope by moving to index.html
+  //jjs: wait till load event fires so all resources are available
+  $scope.fixLinks = function() {
+    //jjs: toggles the view of preview v information
+    $('.informationLink').on('click', function(){
+      $('.previewContainer').toggle()
+    });
+    //jjs: reloads state, which looks for a new book in same category
+    $('.reloadLink').on('click', function() {
+      $state.reload();
+    });
   }
+  //jjs: after everything is done, load up the listeners for the bookmarks
+  $scope.fixLinks()
+
 }
-
-
-$("#expand").click(()=> {
-  $(".landing").animate({
-    height: "100%"
-  }), 600
-})
-$("#collapse").click(()=> {
-  $(".landing").animate({
-    height: "200px"
-  }), 600
-})
